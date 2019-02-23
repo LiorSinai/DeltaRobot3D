6a615dc82928c6526b259272604d89e88a9562fc
Lior Sinai <sinailior@gmail.com>
2019-02-23 12:44:45 +0100

    Accidently excluded from the first commit

---
+++ calculate_forces_fast.m
@@ -0,0 +1,73 @@
+function [forces,moments] = calculate_forces_fast(R_L1_t,R_L2_t,R_L3_t,thetaA,time_range,L,lambda,indQ,indPhi,r)
+    % Use the Lagrange multiplier method to find the forces at a point
+    % where the contraint equation in Phi are applied
+    N=size(thetaA,2);
+    
+    forces=zeros(3,N);
+    moments=forces;
+    
+    % Note unlike in the main code, it is assumed variables are numeric
+    % E.g. R_L1 is a numeric matrix
+    % unless stated otherwise with _sym
+    
+    % indQ = indices of the general co-ordinates Q variables. Should be 6
+    % consecutives for 3 x,y,z and 3 angles 
+    
+    % indPhi = indices of the relevant constraints in Phi. Should be two
+    % sets of 3 consecutive numbers.
+    
+    % r = vector from the CoM co-ordinate to the contraint co-ordinate
+    
+    
+    fprintf('%d time steps. Progess of forces: 000.0%%\n',N)
+    for timeStep = 1:N
+        %fprintf('% d ... %.1f%% complete of forces\n',timeStep,100*timeStep/length(time_range))
+        fprintf('\b\b\b\b\b\b\b')  %delete previous number and new line
+        fprintf('%05.1f%%\n',timeStep/N*100); %write the new number
+              
+        % Match R_L symbols to numeric values
+        % R_L1
+        R_L1=R_L1_t(:,:,timeStep);
+        rL1_1_1 = R_L1(1,1); rL1_1_2 = R_L1(1,2); rL1_1_3 = R_L1(1,3);
+        rL1_2_1 = R_L1(2,1); rL1_2_2 = R_L1(2,2); rL1_2_3 = R_L1(2,3);
+        rL1_3_1 = R_L1(3,1); rL1_3_2 = R_L1(3,2); rL1_3_3 = R_L1(3,3);
+
+        % R_L2
+        R_L2=R_L2_t(:,:,timeStep);
+        rL2_1_1 = R_L2(1,1); rL2_1_2 = R_L2(1,2); rL2_1_3 = R_L2(1,3);
+        rL2_2_1 = R_L2(2,1); rL2_2_2 = R_L2(2,2); rL2_2_3 = R_L2(2,3);
+        rL2_3_1 = R_L2(3,1); rL2_3_2 = R_L2(3,2); rL2_3_3 = R_L2(3,3);
+
+        % R_L3
+        R_L3=R_L3_t(:,:,timeStep);
+        rL3_1_1 = R_L3(1,1); rL3_1_2 = R_L3(1,2); rL3_1_3 = R_L3(1,3);
+        rL3_2_1 = R_L3(2,1); rL3_2_2 = R_L3(2,2); rL3_2_3 = R_L3(2,3);
+        rL3_3_1 = R_L3(3,1); rL3_3_2 = R_L3(3,2); rL3_3_3 = R_L3(3,3);
+
+        thetaA1=thetaA(1,timeStep);
+        thetaA2=thetaA(2,timeStep);
+        thetaA3=thetaA(3,timeStep);
+
+        J = Jacobian_Numeric(L(2),L(1),...
+        rL1_1_1,rL1_1_2,...%R_L1
+        rL1_2_1,rL1_2_2,...
+        rL1_3_1,rL1_3_2,...
+        rL2_1_1,rL2_1_2,...%R_L2
+        rL2_2_1,rL2_2_2,...
+        rL2_3_1,rL2_3_2,...
+        rL3_1_1,rL3_1_2,...%R_L3
+        rL3_2_1,rL3_2_2,...
+        rL3_3_1,rL3_3_2,...
+        thetaA1,thetaA2,thetaA3);
+        
+        % equation 6.43 on pg 71 of the notes
+        lambda_i=lambda(indPhi,timeStep);
+        Phi_ri    =J(indPhi,indQ(1:3));
+        Phi_thetai=J(indPhi,indQ(4:6));
+%         Phi_ri    =J(:,1:3);
+%         Phi_thetai=J(:,4:6);   
+        forces(:,timeStep)   =-Phi_ri.'*lambda_i;
+        moments(:,timeStep)=-Phi_thetai.'*lambda_i-tilde(r(:,timeStep))*forces(:,timeStep);
+    end
+end
+
